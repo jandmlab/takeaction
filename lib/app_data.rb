abac244bfff08d0ff3dd11b1610d05ec4f52ffd7
Jeff Mueller <jnmueller@me.com>
2014-02-18 13:12:31 -0800

    Added ability to customize reminder prefix text

--- lib/app_data.rb
+++ lib/app_data.rb
@@ -1,5 +1,12 @@
 class AppData
-  FIELDS = [:app, :emoji, :twitterhandle, :clearlist, :commandcmac, :commandciphone, :commandcipad, :customdraftsactionname, :pythonistascriptname, :twodayspage, :taskagentlist]
+  FIELDS = [
+    :app, :emoji, :twitterhandle, :clearlist, :commandcmac, 
+    :commandciphone, :commandcipad, :customdraftsactionname, 
+    :pythonistascriptname, :twodayspage, :taskagentlist, 
+    :twodaysprefix, :clearprefix, :dueprefix, :fantasticalprefix, 
+    :taskagentprefix, :thingsprefix
+  ]
+  
   FIELDS.each do |field|
     attr_accessor field
   end
@@ -18,17 +25,17 @@ class AppData
   
   def url_schemes
     {
-      twodays: "2 Days=twodays://x-callback-url/add?page={{#{twodayspage}}}&text={{Check out '+t+': '+location.href+'}}",
+      twodays: "2 Days=twodays://x-callback-url/add?page={{#{twodayspage}}}&text={{#{twodaysprefix}'+t+': '+location.href+'}}",
       buffer: "Buffer=bufferapp://?u={{'+location.href+'}}",
       cleanlinks: "Clean Link=clean-links://x-callback-url/clean?url={{'+location.href+'}}",
-      clear: "Clear=clearapp://task/create?listName={{#{clearlist}}}&listPosition=&taskName={{Check out '+t+': '+location.href+'}}&taskPosition=Top",
+      clear: "Clear=clearapp://task/create?listName={{#{clearlist}}}&listPosition=&taskName={{#{clearprefix}'+t+': '+location.href+'}}&taskPosition=Top",
       commandcipad: "Command-C to iPad=command-c://x-callback-url/copyText?text={{'+window.location+'}}&x-success={{'+window.location+'}}&x-failure={{'+window.location+'}}&deviceName={{#{commandcipad}}}",
       commandciphone: "Command-C to iPhone=command-c://x-callback-url/copyText?text={{'+window.location+'}}&x-success={{'+window.location+'}}&x-failure={{'+window.location+'}}&deviceName={{#{commandciphone}}}",
       commandcmac: "Command-C to Mac=command-c://x-callback-url/copyText?text={{'+window.location+'}}&x-success={{'+window.location+'}}&x-failure={{'+window.location+'}}&deviceName={{#{commandcmac}}}",
       drafts: "Drafts=drafts://x-callback-url/create?text={{'+location.href+'}}",
       draftscustom: "#{customdraftsactionname}=drafts://x-callback-url/create?text={{'+location.href+'}}&action={{#{customdraftsactionname}}}",
-      due: "Due=due://x-callback-url/add?title={{Check out '+t+': '+location.href+'}}",
-      fantastical: "Reminder=fantastical2://parse?sentence={{'+location.href+'}}&reminder=1",
+      due: "Due=due://x-callback-url/add?title={{#{dueprefix}'+t+': '+location.href+'}}",
+      fantastical: "Reminder=fantastical2://parse?sentence={{#{fantasticalprefix}'+t+': '+location.href+'}}&reminder=1",
       instapaper: "Instapaper=x-callback-instapaper://x-callback-url/add?url={{'+location.href+'}}",
       onepassword: "Open in 1Password=op'+location.href+'",
       pincase: "Pincase=pincaseapp://x-callback-url/add?url={{'+location.href+'}}&title={{'+t+'}}&noui=yes&later=no",
@@ -37,9 +44,9 @@ class AppData
       pushpin: "Pushpin=pushpin://x-callback-url/add?url={{'+location.href+'}}&title={{'+t+'}}",
       pythonista: "#{pythonistascriptname}=pythonista://{{#{pythonistascriptname}}}?action=run&args={{'+location.href+'}}",
       riposte: "Riposte=riposte://x-callback-url/createNewPost?text=[{{'+t+'}}]({{'+location.href+'}})",
-      taskagent: "Task Agent=taskagent://x-callback-url/action?list={{#{taskagentlist}}}&task={{Check out '+t+': '+location.href+'}}",
+      taskagent: "Task Agent=taskagent://x-callback-url/action?list={{#{taskagentlist}}}&task={{#{taskagentprefix}'+t+': '+location.href+'}}",
       texttool: "Encode Link=texttool://x-callback-url/transform?text={{'+location.href+'}}&method=encode&x-success={{launch:}}",
-      things: "Things=things:add?title={{Check out '+t+'}}&notes={{'+location.href+'}}",
+      things: "Things=things:add?title={{#{thingsprefix}'+t+'}}&notes={{'+location.href+'}}",
       tumblr: "Tumblr Link Post=tumblr://x-callback-url/link?title={{'+t+'}}&url={{'+location.href+'}}",
       tweetbot: "Tweetbot=tweetbot://{{#{twitterhandle}}}/post?text={{'+location.href+'}}"
     }
--- public/js/actionmenu.js
+++ public/js/actionmenu.js
@@ -9,6 +9,10 @@ $(document).ready(function() {
       $(this).parent().children('.notes').children('.well').css('display', 'none');
       $(this).parent().children('.notes').children('.' + $(this).val()).css('display', 'block');
     });
+    
+    $('input.prefix').keyup(function() {
+      $(this).parent().children('.note').children('.label').children('.prefix-preview').html($(this).val());
+    });
   }
 
 });
